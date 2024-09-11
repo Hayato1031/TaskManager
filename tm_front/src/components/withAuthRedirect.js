af72d05b454d0e9c6ac531b4033197ad6e8bd030
Hayato1031 <nakamurahayato1031@keio.jp>
2024-09-11 21:59:54 +0900

    Update: Auth

--- tm_front/src/components/withAuthRedirect.js
+++ tm_front/src/components/withAuthRedirect.js
@@ -4,7 +4,8 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const withAuthRedirect = (WrappedComponent, redirectTo = "/") => {
-  return (props) => {
+  // WrappedComponentをラップするWithAuthRedirectコンポーネント
+  const WithAuthRedirect = (props) => {
     const { isLoggedIn } = useAuth();
     const router = useRouter();
 
@@ -12,7 +13,7 @@ const withAuthRedirect = (WrappedComponent, redirectTo = "/") => {
       if (isLoggedIn) {
         router.replace(redirectTo);  // ログイン済みならリダイレクト
       }
-    }, [isLoggedIn, router]);
+    }, [isLoggedIn, router, redirectTo]);
 
     if (isLoggedIn) {
       return null; // ログイン済みなら何も表示しない
@@ -20,6 +21,11 @@ const withAuthRedirect = (WrappedComponent, redirectTo = "/") => {
 
     return <WrappedComponent {...props} />;
   };
+
+  // displayNameを設定
+  WithAuthRedirect.displayName = `withAuthRedirect(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+  return WithAuthRedirect;
 };
 
 export default withAuthRedirect;
